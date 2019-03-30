3f99aa3c3f70d97ee8fa8d979c87c942842c7034
aghnaz <aghna_zhalila_26rpl@student.smktelkom-mlg.sch.id>
2019-03-30 14:29:43 +0700

    First Commit

---
+++ app/src/main/java/id/smktelkom/sch/moneyplaned/MainActivity.kt
@@ -0,0 +1,24 @@
+package id.smktelkom.sch.moneyplaned
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        FirebaseInstanceId.getInstance().instanceId
+            .addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    return@OnCompleteListener
+                }
+                val token = task.result!!.token
+                Toast.makeText(this@MainActivity, token, Toast.LENGTH_SHORT).show()
+            })
+    }
+}
